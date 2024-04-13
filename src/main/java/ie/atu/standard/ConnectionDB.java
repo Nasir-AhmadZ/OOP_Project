d817a1cba6e29726c47ce77b9a120b5fa7055022
David <G00422935@atu.ie>
2024-04-14 00:14:24 +0100

    Connecting sql db to intellij

---
+++ src/main/java/ie/atu/standard/ConnectionDB.java
@@ -0,0 +1,34 @@
+package ie.atu.standard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        try
+        {
+            // Load the driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection to the database, hardcoding values for now.
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/electronicstore", "root", "password!");
+            System.out.println("Connection to the sql db was successful");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally
+        {
+            // Close the connection when finished
+            if (connection != null) {
+                try
+                {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
