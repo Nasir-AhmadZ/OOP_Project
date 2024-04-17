5005295b8e640bea2a5a793e9204f018e3f4a758
Nasir Ahmad <g00419630@atu.ie>
2024-04-17 01:26:06 +0100

    Added the method login which lets the user to login or register

---
+++ src/main/java/ie/atu/Login.java
@@ -0,0 +1,96 @@
+package ie.atu;
+import java.sql.*;
+import java.util.Scanner;
+public class Login {
+
+    private String TempName;
+    String url = "jdbc:mysql://localhost:3306/electronicstore";
+    String username = "root";
+    String password = "password";
+    public Login(){
+        this.TempName = "Igor";
+    }
+
+    public String getTempName() {
+        return TempName;
+    }
+
+    public void setTempName(String tempName) {
+        TempName = tempName;
+    }
+
+    public void getUserInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your name to login");
+        this.TempName=scanner.nextLine();
+    }
+
+    public int signed()
+    {
+        int id=0;
+
+        String selectSQL = "SELECT id, age, email " +
+                "FROM customer where name = '" + this.TempName + "'";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectSQL)) {
+
+            while (resultSet.next()) {
+                id = resultSet.getInt("id");
+                int age = resultSet.getInt("age");
+                String email = resultSet.getString("email");
+
+                System.out.println("Id: " + id + ", Age: " + age + ", Email: " + email);
+                System.out.println("Successfully Logged in");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if(id!=0)
+        {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public void register()
+            throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        String tempName, tempEmail;
+        int tempAge;
+        System.out.println("Please enter your email");
+        tempEmail = scanner.nextLine();
+        System.out.println("Please enter your name");
+        tempName = scanner.nextLine();
+        System.out.println("Please enter your age");
+        tempAge = scanner.nextInt();
+
+        // Connect to the database
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/electronicstore", "root", "password");
+
+
+        try {
+
+            // Insert a new record into the "users" table
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO customer (name, age,email) VALUES (?, ?, ?)");
+            stmt.setString(1, tempName);
+            stmt.setInt(2, tempAge);
+            stmt.setString(3, tempEmail);
+            stmt.executeUpdate();
+
+            System.out.println("Insert completed successfully.");
+        } catch (SQLException ex) {
+
+            System.out.println("Record insert failed.");
+            ex.printStackTrace();
+        }
+        // Close the connection
+        conn.close();
+    }
+
+}
