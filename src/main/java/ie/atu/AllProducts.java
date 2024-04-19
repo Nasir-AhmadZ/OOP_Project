7977bb3abcf5289fffc9d17d5c3e21d516d097f3
Nasir Ahmad <g00419630@atu.ie>
2024-04-19 03:32:32 +0100

    Added some more code to All Products

--- src/main/java/ie/atu/AllProducts.java
+++ src/main/java/ie/atu/AllProducts.java
@@ -1,5 +1,7 @@
 package ie.atu;
 
+import java.sql.*;
+
 public class AllProducts {
     public String url;
     public String username;
@@ -9,5 +11,22 @@ public class AllProducts {
         url = "jdbc:mysql://localhost:3306/electronicstore";
         username = "root";
         password = "password";
+    }
+
+    public void brand()
+    {
+        String selectSQL = "SELECT brand " + "from products";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectSQL)) {
+
+            while (resultSet.next()) {
+                String prodCat = resultSet.getString("brand");
+                System.out.println(prodCat);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
